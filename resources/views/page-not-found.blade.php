e41314870f74a5b1abd939deed5332720e94c270
cur1ousFranz <xfranjeff@gmail.com>
2023-02-12 00:54:18 +0800

    create 404 page

---
+++ resources/views/page-not-found.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Edukar | Notifications">
+    <x-container>
+        <div class="text-center py-5">
+            <h1 class="mt-5 text-secondary" style="font-size: 50px">404</h1>
+            <h2 class="mt-2 text-secondary">Ooops! Page not found.</h1>
+            <button class="btn bg-secondary text-white" onclick="history.back()">Get back</button>
+        </div>
+    </x-container>
+</x-layout>
