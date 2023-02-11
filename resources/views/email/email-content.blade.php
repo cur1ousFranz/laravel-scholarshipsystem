7d692b690ac27b7b8ff10b4886798cc4d6af0ff7
cur1ousFranz <xfranjeff@gmail.com>
2023-02-12 00:56:04 +0800

    include layout of email body

--- resources/views/email/email-content.blade.php
+++ resources/views/email/email-content.blade.php
@@ -1,1 +1,40 @@
-{!! $body !!}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add To Cart</title>
+    <style>
+        .container {
+            width: 800px;
+            margin: 0 auto;
+            border: 1px solid;
+            padding-right: 30px;
+            padding-left: 30px;
+            padding-bottom: 10px;
+            padding-top: 10px;
+        }
+
+        h1, h3 {
+            color: black;
+        }
+
+        body {
+            background-color: rgb(215, 221, 221)
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <h1 style="font-weight: 25px; margin-bottom: 10px">Scholarship Announcement</h1>
+        <p>
+            {!! $body !!}
+        </p>
+        <p class="mt-3">Edukar Gensan Scholarship</p>
+    </div>
+
+</body>
+</html>
+
